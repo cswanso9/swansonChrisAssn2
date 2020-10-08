6076a6e17170a92f5b7c4e88d10e1724192f07b6
chris <chris@DESKTOP-5CN0M8I>
2020-10-07 22:21:55 -0700

    First commit

---
+++ SwansonChris-assn2/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,56 @@
+package cse360assignment02;
+/*<h1>Adding Machine</h1>
+ * This program doesn't do much right now.
+ * 
+ * @author cse360
+ * 
+ */
+
+public class AddingMachine {
+	private int total;
+	  
+	/*
+	 * The constructor only instantiates and object and sets the initial total to 0.
+	 */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /*
+	   * This method just returns 0.
+	   * @return int It's 0.
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /*
+	   * This method doesn't do anything.
+	   * @param value Unused.
+	   * @return Nothing.
+	   */
+	  public void add (int value) {
+	  }
+
+	  /*
+	   * This method doesn't do anything.
+	   * @param value Unused.
+	   * @return Nothing.
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /*
+	   * This method just returns an empty string.
+	   * @return String An empty string.
+	   */
+	  public String toString () {
+	    return "";
+	  }
+
+	  /*
+	   * This method doesn't do anything.
+	   */
+	  public void clear() {
+	  }
+}
